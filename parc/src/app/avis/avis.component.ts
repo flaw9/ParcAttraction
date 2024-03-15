3accd280c9d885007a6159d7cbc015da5e72547e
Gabriel Atgie moro <120777814+Deatwarior@users.noreply.github.com>
2024-03-15 16:16:16 +0100

    Update avis.component.ts
    ajout d'une fonction pour avoir les critiques

--- parc/src/app/avis/avis.component.ts
+++ parc/src/app/avis/avis.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './avis.component.html',
   styleUrl: './avis.component.scss'
 })
+    getCritiques(): void {
+    this.attractionService.getCritique(this.attraction_id ?? -1).subscribe(critiques => {
+      this.critiques = critiques.critiques;
+      this.mean = critiques.mean;
+    });
+  }
 export class AvisComponent {
 
 }
